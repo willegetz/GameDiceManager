4c59ba641e58d92c7a5f9056c43b2160d1b546dd
William Getz <willegetz@gmail.com>
2021-04-15 20:00:10 -0700

    Renamed project, added first test file

---
+++ GameDiceManager/DiceTypeTests/D6Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DiceTypeTests
+{
+    [TestClass]
+    public class D6Tests
+    {
+        [TestMethod]
+        public void D6_ListItsNumbers()
+        {
+            // A six sided die contains numbers from 1 to 6
+            // A person can pick up the die and see all the numbers
+            //  by rotating it in their hand
+        }
+    }
+}
---
+++ GameDiceManager/DiceTypeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DiceTypeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiceTypeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ae48131f-c531-4d85-9350-0ddf3f05ab4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
