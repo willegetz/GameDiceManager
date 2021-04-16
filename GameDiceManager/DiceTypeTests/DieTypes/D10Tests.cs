48830d0f9bb3cba4fc6e46cfa0b0d74b5fa298a4
William Getz <willegetz@gmail.com>
2021-04-15 22:51:53 -0700

    Start adding D10 functionality

---
+++ GameDiceManager/DiceTypeTests/DieTypes/D10Tests.cs
@@ -0,0 +1,19 @@
+﻿using DiceTypes.DieTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiceTypeTests.DieTypes
+{
+    [TestClass]
+    public class D10Tests
+    {
+        [TestMethod]
+        public void ReportsItsType()
+        {
+            var d10 = new D10(0);
+            var dieType = d10.GetDieType();
+
+            var expectedDieType = "D10";
+            Assert.AreEqual(expectedDieType, dieType);
+        }
+    }
+}
---
+++ GameDiceManager/DiceTypes/DieTypes/D10.cs
@@ -0,0 +1,17 @@
+﻿namespace DiceTypes.DieTypes
+{
+    public class D10
+    {
+        private readonly Die _d10;
+
+        public D10(int randomSeed)
+        {
+            _d10 = new Die(10, randomSeed);
+        }
+
+        public string GetDieType()
+        {
+            return "D10";
+        }
+    }
+}
