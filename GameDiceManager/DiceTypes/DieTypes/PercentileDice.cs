fbf5872eab92e517c622bd4b24334eeacace3693
William Getz <willegetz@gmail.com>
2021-04-16 18:21:29 -0700

    Tens die is now being used

--- GameDiceManager/DiceTypes/DieTypes/PercentileDice.cs
+++ GameDiceManager/DiceTypes/DieTypes/PercentileDice.cs
@@ -4,6 +4,13 @@ namespace DiceTypes.DieTypes
 {
     public class PercentileDice
     {
+        private Die _tensDie;
+
+        public PercentileDice()
+        {
+            _tensDie = new Die(10, 5);
+        }
+
         public string GetDieType()
         {
             return "Percentile (2D10)";
@@ -11,13 +18,17 @@ namespace DiceTypes.DieTypes
 
         public string GetFaceNumbers()
         {
-            return @"Tens Die: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            var tensFaceNumbers = $"Tens Die: {_tensDie.ListFaceNumbers()}";
+            return $@"{tensFaceNumbers}
 Ones Die: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
         }
 
         public int RollDice()
         {
-            return 48;
+            var tensResult = _tensDie.RollDie();
+            var result = Convert.ToInt32($"{tensResult}8");
+
+            return result;
         }
     }
 }
