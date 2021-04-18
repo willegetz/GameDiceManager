3bd2de6cd61f983b22da65a6f7686ef1d7958416
William Getz <willegetz@gmail.com>
2021-04-17 19:25:53 -0700

    Mordheim D66 die type started

---
+++ GameDiceManager/DiceTypeTests/DieTypes/D66Tests.cs
@@ -0,0 +1,22 @@
+﻿using DiceTypes.DieTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DiceTypeTests.DieTypes
+{
+    [TestClass]
+    public class D66Tests
+    {
+        [TestMethod]
+        public void ReportsItsType()
+        {
+            var d66 = new D66();
+
+            var dieType = d66.GetDieType();
+
+            var expectedDieType = "D66 (2D6)";
+
+            Assert.AreEqual(expectedDieType, dieType);
+        }
+    }
+}
---
+++ GameDiceManager/DiceTypes/DieTypes/D66.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiceTypes.DieTypes
+{
+    public class D66
+    {
+        public string GetDieType()
+        {
+            return "D66 (2D6)";
+        }
+    }
+}
