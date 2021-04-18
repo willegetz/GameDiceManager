7522b0b1ad1fb348d83bc314690e08aa0c087749
William Getz <willegetz@gmail.com>
2021-04-17 19:34:48 -0700

    Introduced ones die

--- GameDiceManager/DiceTypes/DieTypes/D66.cs
+++ GameDiceManager/DiceTypes/DieTypes/D66.cs
@@ -5,10 +5,12 @@ namespace DiceTypes.DieTypes
     public class D66
     {
         private Die _tensDie;
+        private Die _onesDie;
 
         public D66()
         {
             _tensDie = new Die(6, 0);
+            _onesDie = new Die(6, 0);
         }
 
         public string GetDieType()
@@ -19,9 +21,10 @@ namespace DiceTypes.DieTypes
         public string GetFaceNumbers()
         {
             var tensFaces = _tensDie.ListFaceNumbers();
+            var onesFaces = _onesDie.ListFaceNumbers();
 
             return $@"Tens Die: {tensFaces}
-Ones Die: 1, 2, 3, 4, 5, 6";
+Ones Die: {onesFaces}";
         }
 
         public int RollDie()
