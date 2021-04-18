b2c9b0feef18878349f2d05932b06325b746be63
William Getz <willegetz@gmail.com>
2021-04-17 19:53:04 -0700

    Face number scaffold

--- GameDiceManager/DiceTypeTests/DieTypes/D3TypeATests.cs
+++ GameDiceManager/DiceTypeTests/DieTypes/D3TypeATests.cs
@@ -16,5 +16,15 @@ namespace DiceTypeTests.DieTypes
             var expectedDieType = "D3 Type A (D6 result / 2 rounded up)";
             Assert.AreEqual(expectedDieType, dieType);
         }
+
+        [TestMethod]
+        public void ReportsItsFaces()
+        {
+            var d3TypeA = new D3TypeA();
+            var faceNumbers = D3TypeA.GetFaceNumbers();
+
+            var expectedFaceNumbers = "1, 2, 3, 4, 5, 6";
+            Assert.AreEqual(expectedFaceNumbers, faceNumbers);
+        }
     }
 }
--- GameDiceManager/DiceTypes/DieTypes/D3TypeA.cs
+++ GameDiceManager/DiceTypes/DieTypes/D3TypeA.cs
@@ -1,10 +1,17 @@
-﻿namespace DiceTypes.DieTypes
+﻿using System;
+
+namespace DiceTypes.DieTypes
 {
     public class D3TypeA
     {
         public string GetDieType()
         {
             return "D3 Type A (D6 result / 2 rounded up)";
+        }
+
+        public static object GetFaceNumbers()
+        {
+            return "1, 2, 3, 4, 5, 6";
         }
     }
 }
