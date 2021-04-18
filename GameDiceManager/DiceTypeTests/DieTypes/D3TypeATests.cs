bf78b89a629baa8f345bbf4265ad205be02bc765
William Getz <willegetz@gmail.com>
2021-04-17 19:50:15 -0700

    D3 Type A scaffold started

---
+++ GameDiceManager/DiceTypeTests/DieTypes/D3TypeATests.cs
@@ -0,0 +1,20 @@
+﻿using DiceTypes.DieTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DiceTypeTests.DieTypes
+{
+    [TestClass]
+    public class D3TypeATests
+    {
+        [TestMethod]
+        public void ReportsItsType()
+        {
+            var d3TypeA = new D3TypeA();
+            var dieType = d3TypeA.GetDieType();
+
+            var expectedDieType = "D3 Type A (D6 result / 2 rounded up)";
+            Assert.AreEqual(expectedDieType, dieType);
+        }
+    }
+}
---
+++ GameDiceManager/DiceTypes/DieTypes/D3TypeA.cs
@@ -0,0 +1,10 @@
+﻿namespace DiceTypes.DieTypes
+{
+    public class D3TypeA
+    {
+        public string GetDieType()
+        {
+            return "D3 Type A (D6 result / 2 rounded up)";
+        }
+    }
+}
