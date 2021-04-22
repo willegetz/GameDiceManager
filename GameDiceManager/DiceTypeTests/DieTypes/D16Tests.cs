674767c319d949fde646aa7c86360fccd4dbdd63
William Getz <willegetz@gmail.com>
2021-04-22 05:52:27 -0700

    Added D16

---
+++ GameDiceManager/DiceTypeTests/DieTypes/D16Tests.cs
@@ -0,0 +1,27 @@
+﻿using DiceTypes.DieTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiceTypeTests.DieTypes
+{
+    // From World Builder's Guidebook
+    // In orrder to generate a d16, roll a d6 and a d8 simultaneously.
+    // If the d6 comes up 1-3, read the d8 without modification.
+    // If the d6 comes up 4-6, read the d8 as d8+8.
+    // This gernerates a random number between 1 and 16.
+
+    [TestClass]
+    public class D16Tests
+    {
+        [TestMethod]
+        public void ReportsItsType()
+        {
+            var d16 = new D16();
+            var dieType = d16.GetDieType();
+
+            var expectedDieType = "D16";
+
+            Assert.AreEqual(expectedDieType, dieType);
+
+        }
+    }
+}
---
+++ GameDiceManager/DiceTypes/DieTypes/D16.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceTypes.DieTypes
+{
+    public class D16
+    {
+        public D16()
+        {
+
+        }
+
+        public string GetDieType()
+        {
+            return "D16";
+        }
+
+        public string GetFaceNumbers()
+        {
+            return "";
+        }
+
+        public int RollDice()
+        {
+            return -1;
+        }
+    }
+}
