368bcc3ed255d11a30ec24718d356836e8c860d8
William Getz <willegetz@gmail.com>
2021-04-23 16:13:23 -0700

    Add D12

---
+++ GameDiceManager/DiceTypeTests/DieTypes/D12Tests.cs
@@ -0,0 +1,42 @@
+﻿using DiceTypes.DieTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DiceTypeTests.DieTypes
+{
+    [TestClass]
+    public class D12Tests
+    {
+        [TestMethod]
+        public void ReportItsType()
+        {
+            var d12 = new D12(0);
+            var dieType = d12.GetDieType();
+
+            var expectedDieType = "D12";
+            Assert.AreEqual(expectedDieType, dieType);
+        }
+
+        [TestMethod]
+        public void ReportItsFaceNumbers()
+        {
+            var d12 = new D12(0);
+            var faceNumbers = d12.GetFaceNumbers();
+
+            var expectedFaceNumbers = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12";
+            Assert.AreEqual(expectedFaceNumbers, faceNumbers);
+        }
+
+        [TestMethod]
+        public void Returns11WhenRolled()
+        {
+            var randomSeed = 6;
+
+            var d12 = new D12(randomSeed);
+            var rollResult = d12.RollDie();
+
+            var expectedRollResult = 11;
+            Assert.AreEqual(expectedRollResult, rollResult);
+        }
+    }
+}
---
+++ GameDiceManager/DiceTypes/DieTypes/D12.cs
@@ -0,0 +1,27 @@
+﻿namespace DiceTypes.DieTypes
+{
+    public class D12
+    {
+        private Die _d12;
+
+        public D12(int randomSeed)
+        {
+            _d12 = new Die(12, randomSeed);
+        }
+
+        public string GetDieType()
+        {
+            return "D12";
+        }
+
+        public string GetFaceNumbers()
+        {
+            return _d12.ListFaceNumbers();
+        }
+
+        public int RollDie()
+        {
+            return _d12.RollDie();
+        }
+    }
+}
