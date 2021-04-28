77257cf019b6c3203cf14881946d96092782552f
William Getz <willegetz@gmail.com>
2021-04-28 11:53:51 -0700

    Add D4

---
+++ GameDiceManager/DiceTypeTests/DieTypes/D4Tests.cs
@@ -0,0 +1,30 @@
+﻿using DiceTypes.DieTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DiceTypeTests.DieTypes
+{
+    [TestClass]
+    public class D4Tests
+    {
+        [TestMethod]
+        public void ReportsItsType()
+        {
+            var d6 = new D4();
+            var dieType = d6.GetDieType();
+
+            var expectedDieType = "D4";
+            Assert.AreEqual(expectedDieType, dieType);
+        }
+
+        [TestMethod]
+        public void ReportItsFaceNumbers()
+        {
+            var d6 = new D4();
+            var faceNumbers = d6.GetFaceNumbers();
+
+            var expectedFaceNumbers = "1, 2, 3, 4";
+            Assert.AreEqual(expectedFaceNumbers, faceNumbers);
+        }
+    }
+}
---
+++ GameDiceManager/DiceTypes/DieTypes/D4.cs
@@ -0,0 +1,22 @@
+﻿using DiceTypes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceTypes.DieTypes
+{
+    public class D4 : BaseDie, IDie
+    {
+        public D4(): base(4)
+        {
+
+        }
+
+        public string GetDieType()
+        {
+            return "D4";
+        }
+    }
+}
